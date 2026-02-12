b80ddd0b0c7b8df00c2b11a372e6ac03edb60df8
Peter-Zheng1 <343277125@tdsb.ca>
2026-02-12 16:29:44 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,12 @@
+	/*
+* File name: Work
+* Author: Peter Zheng
+* Date Created: Feb 21 2026
+* Date last modified: Feb 21 2026
+  */
+
 public class PracticeProblem {
+
 
 	public static void main(String args[]) {
 		q1();
@@ -9,18 +17,34 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
+
 	}
+	
 
 	public static void q2() {
 		//Write question 2 code here
+	String cat = ("          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''");
+	System.out.println(cat);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String fn = ("Fortnite");
+		String Awful = (" is awful");
+
+		System.out.print(fn);
+		System.out.println(Awful);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String Dont = ("Don't");
+		String At = ("@ me");
+
+		System.out.println(Dont + " " + At);
 	}
 
 
