8216d29dbb869080f8ca2335c77066cc4d68c81a
James Kalisz <james.kalisz@tdsb.on.ca>
2026-02-12 10:45:38 -0500

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,7 +2,7 @@
 * File Name: Lesson 1.3 - Strings
 * Author: Mr. Kalisz
 * Date Created: Feb 8, 2023
-* Date Last Modified: Feb 8, 2023
+* Date Last Modified: Feb 12, 2026
 */
 
 class Notes  {
@@ -11,45 +11,62 @@ class Notes  {
 		// Documentation/multi line comments
 
 		/*
-This is a multi line comment
-HEre is a comment
-This is a comment too
-And this
-And this
-		*/ // The "*/" ended my multi comment
+			This is a multi line comment
+			HEre is a comment
+			This is a comment too
+			And this
+			And this
+		*/ 
+		// The "*/" ended the multi comment
 
 		/**
-			* These are documentation
-			*
-			*
-			*/
+		  * These are documentation
+		  *
+		  *
+		  */
+		
+		//Output
+
 		
 		// print - does not add a new line after the output
 
-		System.out.print("Hello World");
-		System.out.println("Hello World"); //println does add a new line after the output
+		System.out.println("Hello World"); //println adds a new line after the output
+		System.out.print("Hello World"); //print does not
+		System.out.println("I'm on the same line"); //same line since the previous print did not add a new line
 		
 		// newline character - "\n"
+		
+		//must be used inside quotation marks otherwise the new line character does nothing and crashes
+		// System.out.println("Hello" \n);
+		System.out.println("Hello\n\n\n\nWorld"); 
 
-		System.out.println("Hello\n\n\n\nWorld");
-
-		System.out.println("a" + "\n" + "b"); //must be used inside quotation marks otherwise the new line character does nothing
 		
 		// Strings
-
-		String word;
+		
+		String word; //String has a capital S, not lowercase
 		word = "Hello";
-
+		
 		System.out.println(word);
 		System.out.println("word");
-
-		//concatenation
-
+		
+		//concatenation - combining strings
+		
 		String word2;
 		word2 = "Help Me";
-
-		System.out.println(word + word2); //has no space between them
-		System.out.println(word + " " + word2); //add a space concatenated inbetween the two variables to separate them.
 		
+		String word3;
+		word3 = word + word2; //has no space between them
+		System.out.println(word3);
+		
+		word3 = word + " " + word2; //has no space between them
+		System.out.println(word3); //add a space concatenated inbetween the two variables to separate them.
+		
+		word2 = word2 + "."; //Similar to integers we can append(add to the end) to the Strings
+		System.out.println(word2);
+		
+		System.out.println("a" + "\n" + "b"); 
+
+		//Concatenation is done when adding strings with other data types
+		System.out.println(word2 + 5 + 4);
 	}
 }
